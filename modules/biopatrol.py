8e4a0b2e50cf269e112be44f2a203fe22db37079
Evil Horse <rainbowdysch13@gmail.com>
2024-09-02 19:54:15 +0300

    biopatrol overhaul:
    1) convert data to appropriate format and store it separately
    2) track CMDR's own found biosignals
    3) when scanned, remove others with matching genus from the planet
    4) when scanned, check if everything is found and remove all occurences
       if so
    5) use coords from event data if event.coords contains None
       it uses EDSM API to fetch coordinates for current system, could
       return nothing if CMDR is discoverer

--- modules/biopatrol.py
+++ modules/biopatrol.py
@@ -19,7 +19,9 @@ def distance_between(a: Coords, b: Coords):
 
 
 class BioPatrol(nb.Frame, Module):
-    FILENAME = 'bio.json'
+    FILENAME_RAW  = 'bio.json'
+    FILENAME_FLAT = 'bio-flat.json'
+    FILENAME_BIO  = 'bio-found.json'
 
     def __init__(self, parent, gridrow):
         super().__init__(parent)
@@ -80,23 +82,68 @@ class BioPatrol(nb.Frame, Module):
 
 
     def load_data(self):
+        self.body = None
         try:
-            f = open(Path(self.plugin_dir, "data", self.FILENAME), 'r')
+            f = open(Path(self.plugin_dir, "data", self.FILENAME_BIO), 'r')
+            self.__bio_found = json.load(f)
+        except:
+            self.set_status(f"Данные по находкам не найдены или повреждены (data/{self.FILENAME_BIO})")
+            self.__bio_found = {}
+
+            # {
+            #   "signalCount": 5
+            #   "signals" : [
+            #     "Tussock Pennatis - Yellow",
+            #     "Tussock Pennatis - Yellow",
+            #     "Tussock Pennatis - Yellow",
+            #     "Tussock Pennatis - Yellow",
+            #     "Tussock Pennatis - Yellow"
+            #   ]
+            # }
+
+        try:
+            f = open(Path(self.plugin_dir, "data", self.FILENAME_FLAT), 'r')
             self.__raw_data = json.load(f)
         except:
-            self.set_status(f"Данные по биологии не найдены или повреждены (data/{self.FILENAME})")
-            self._enabled = False
+            self.set_status(f"Данные по биологии не найдены или повреждены (data/{self.FILENAME_FLAT})")
 
+            try:
+                f = open(Path(self.plugin_dir, "data", self.FILENAME_RAW), 'r')
+                raw_data = json.load(f)
+            except:
+                self.set_status(f"Данные по биологии не найдены или повреждены (data/{self.FILENAME_RAW})")
+                self._enabled = False
+
+            self.__raw_data = {}
+            for region, region_data in raw_data.items():
+                for species, species_data in region_data.items():
+                    priority = species_data["priority"]
+                    if species not in self.__raw_data:
+                        self.__raw_data[species] = {"locations" : {}}
+
+                    for location in species_data["locations"]:
+                        location["region"] = region
+                        location["priority"] = priority
+                        body = location["body"]
+                        del location["body"]
+                        self.__raw_data[species]["locations"][body] = location
+
+                self.set_status(f"Обработан регион {region}")
+            self.save_data()
+
+            self.set_status(f"Готов к работе")
 
     def save_data(self):
-        with open(Path(self.plugin_dir, "data", self.FILENAME), 'w') as f:
+        with open(Path(self.plugin_dir, "data", self.FILENAME_FLAT), 'w') as f:
             json.dump(self.__raw_data, f, ensure_ascii=False, indent=2)
-        
-    
+
+        with open(Path(self.plugin_dir, "data", self.FILENAME_BIO), 'w') as f:
+            json.dump(self.__bio_found, f, ensure_ascii=False, indent=2)
+
     def on_journal_entry(self, entry: JournalEntry):
         if not self._enabled:
             return
-        
+
         event = entry.data["event"]
 
         if event in ("Location", "FSDJump"):
@@ -104,23 +151,56 @@ class BioPatrol(nb.Frame, Module):
             self.pos = next((i for i, bio in enumerate(self.data) if bio["species"] == self.selected_bio), 0)
             self.after(0, self.show)
 
-        elif event == "ScanOrganic" and entry.data["Variant_Localised"] in self.get_species_left_to_discover():
+        elif event == "ScanOrganic":
+            genus = entry.data["Genus_Localised"]
             bioname = entry.data["Variant_Localised"]
-            self.__raw_data[bioname]["collected"] = True
+            self.set_status(f"Scanned {bioname} at {self.body}")
+
+            if bioname not in self.__bio_found[self.body]["signals"]:
+                self.__bio_found[self.body]["signals"].append(bioname)
+
+            for species, data in self.__raw_data.items():
+                if self.body in data["locations"]:
+
+                    # everything has been found, clean up
+                    if len(self.__bio_found[self.body]["signals"]) == self.__bio_found[self.body]["signalCount"]:
+                        del data["locations"][self.body]
+                    else:
+                        # something left to find, clear all with matching genus
+                        species_genus = species.split()[0]
+
+                        if genus == species_genus and bioname != species:
+                            del data["locations"][self.body]
             self.save_data()
             self.__update_data(entry)
-            self.pos = 0
+            self.pos = next((i for i, bio in enumerate(self.data) if bio["species"] == self.selected_bio), 0)
+            self.after(0, self.show)
 
         elif event == "SAASignalsFound" and entry.data.get("Genuses") is not None:
             genuses = [i["Genus_Localised"] for i in entry.data["Genuses"]]
+            bodyName = entry.data["BodyName"]
+
+            if bodyName not in self.__bio_found:
+                self.__bio_found[bodyName] = {
+                  "signalCount" : len(genuses),
+                  "signals" : []
+                }
+
             for species, data in self.__raw_data.items():
-                for location in data["locations"]:
-                    if entry.data["BodyName"] == location["body"] and species.split()[0] not in genuses:
-                        data["locations"].remove(location)
-                        self.__update_data(entry)
-                        self.pos = next((i for i, bio in enumerate(self.data) if bio["species"] == self.selected_bio), 0)
+                if bodyName in data["locations"] and species.split()[0] not in genuses:
+                    del data["locations"][bodyName]
+                    self.__update_data(entry)
+                    self.pos = next((i for i, bio in enumerate(self.data) if bio["species"] == self.selected_bio), 0)
+                    self.after(0, self.show)
             self.save_data()
-    
+
+    def on_dashboard_entry(self, cmdr, is_beta, entry):
+        if not "BodyName" in entry:
+            self.body = None
+            return
+
+        if self.body != entry["BodyName"]:
+            self.body = entry["BodyName"]
 
     def set_status(self, text: str):
         self.topframe.pack_forget()
@@ -129,27 +209,27 @@ class BioPatrol(nb.Frame, Module):
         self.__dummy_var.set(text)
         self.dummy_label.pack(side="top", fill="x")
 
-    
+
     def show(self):
         self.dummy_label.pack_forget()
         self.topframe.pack(side="top", fill="x")
         self.priority_label.pack(side="top", fill="x")
         self.bottomframe.pack(side="top", fill="x")
 
-    
+
     @property
     def selected_bio(self) -> str:
         return self.__selected_bio_var.get()
-    
+
     @selected_bio.setter
     def selected_bio(self, value: str):
         self.__selected_bio_var.set(value)
 
-    
+
     @property
     def priority(self):
         return self.__priority
-    
+
     @priority.setter
     def priority(self, value: int):
         self.__priority = value
@@ -162,40 +242,45 @@ class BioPatrol(nb.Frame, Module):
     @property
     def closest_location(self) -> str:
         return self.__closest_location_var.get()
-    
+
     @closest_location.setter
     def closest_location(self, value: str):
         self.__closest_location_var.set(value)
 
-    
+
     @property
     def distance_to_closest(self) -> float:
         return self.__distance
-    
+
     @distance_to_closest.setter
     def distance_to_closest(self, value: float):
         self.__distance = value
         self.__distance_var.set(f"{value:.2f} ly")
-    
+
 
     def get_species_left_to_discover(self):
-        return [bio_item for bio_item, data in self.__raw_data.items() if data["collected"] == False and len(data["locations"]) > 0]
-    
+        return [bio_item for bio_item, data in self.__raw_data.items() if len(data["locations"]) > 0]
+
 
     def __update_data(self, entry: JournalEntry):
         def get_closest(current_coords, locations):
-            closest = min(locations, key=lambda l: distance_between(current_coords, Coords(l["x"], l["y"], l["z"])))
+            closest_key = min(locations, key=lambda l: distance_between(current_coords, Coords(locations[l]["x"], locations[l]["y"], locations[l]["z"])))
+            closest = locations[closest_key]
             coords = Coords(closest["x"], closest["y"], closest["z"])
             distance = distance_between(current_coords, coords)
             system = closest["system"]
-            body = closest["body"]
-            return system, body, distance, coords
+            body = closest_key
+            priority = closest["priority"]
+            return system, body, distance, coords, priority
 
         data = []
         current_coords = entry.coords
+        if None in [entry.coords.x, entry.coords.y, entry.coords.z] and "StarPos" in entry.data:
+            starpos = entry.data["StarPos"]
+            current_coords = Coords(starpos[0], starpos[1], starpos[2])
+
         for bio_item in self.get_species_left_to_discover():
-            priority  = self.__raw_data[bio_item]["priority"]
-            closest_system, closest_body, distance, coords = get_closest(current_coords, self.__raw_data[bio_item]["locations"])
+            closest_system, closest_body, distance, coords, priority = get_closest(current_coords, self.__raw_data[bio_item]["locations"])
             data.append({
                 "species": bio_item,
                 "priority": priority,
@@ -214,7 +299,7 @@ class BioPatrol(nb.Frame, Module):
     @property
     def pos(self):
         return self.__pos
-    
+
     @pos.setter
     def pos(self, value: int):
         # вынужденная обёртка ради потокобезопасности
@@ -232,7 +317,7 @@ class BioPatrol(nb.Frame, Module):
 
     def __prev(self):
         self.pos -= 1
-        
+
 
     def __next(self):
         self.pos += 1
@@ -245,7 +330,7 @@ class BioPatrol(nb.Frame, Module):
             self.prev_button.configure(state="disabled")
         if self.pos == len(self.data)-1:
             self.next_button.configure(state="disabled")
-        
+
 
     def __copy(self):
         copyclip(self.data[self.pos]["_system"])
