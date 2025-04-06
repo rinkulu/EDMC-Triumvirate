ffa272eca81eb9bed7d88fcdbcec1427ec54403e
rinkulu <rinkulukat@gmail.com>
2025-04-06 19:47:54 +0300

    move factions check to a separate submodule

--- modules/bgs/core.py
+++ modules/bgs/core.py
@@ -1,9 +1,9 @@
-import csv
 import requests
 from queue import Queue
 from threading import Lock
+from typing import Any, Callable
 
-from context import GameState, PluginContext
+from context import PluginContext
 from modules.legacy import GoogleReporter
 from modules.lib.journal import JournalEntry
 from modules.lib.module import Module
@@ -19,211 +19,66 @@ _translate = functools.partial(PluginContext._tr_template, filepath=__file__)
 
 class FilterUpdater(Thread):
     REFRESH_TIME = 30 * 60  # s
-    INFLUENCE_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSMyLFfN86QHQ5nMgEk8ZNhQPC00ie5am_MoUUoBpD6WpbEyJAGRWsFinMB9xOP8DpMhXz1i-OOUpto/pub?gid=1142599011&single=true&output=csv"  # noqa: E501
-    ADDITIONAL_SYSTEMS_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSMyLFfN86QHQ5nMgEk8ZNhQPC00ie5am_MoUUoBpD6WpbEyJAGRWsFinMB9xOP8DpMhXz1i-OOUpto/pub?gid=626235075&single=true&output=csv"  # noqa: E501
+    FETCH_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTDY_aCGkppsZVZI_XhjBo_E3dxvGWilsOjpti9bPpqFLHM7Ar47pHfTeeSUfjLW3lI3hzsfy0YVCl7/pub?gid=1163755226&single=true&output=csv"  # noqa: E501
 
-    def __init__(self, filter_instance: 'Filter'):
-        self._filter = filter_instance
+    def __init__(self, callback: Callable[[list[str]], Any]):
+        self._callback = callback
         super().__init__(name="Triumvirate.BGSFilterUpdater")
 
     def do_run(self):
         while True:
-            PluginContext.logger.debug("BGS data updating started.")
-            try:
-                influence_res = requests.get(self.INFLUENCE_URL)
-                influence_res.raise_for_status()
-                systems_res = requests.get(self.ADDITIONAL_SYSTEMS_URL)
-                systems_res.raise_for_status()
-            except requests.RequestException as e:
-                PluginContext.logger.error("Couldn't get the BGS systems data. Exception info:", exc_info=e)
-                self.sleep(self.REFRESH_TIME)
-                continue
-
-            influence_data = self.parse_inf_data(influence_res)
-            if influence_data is None:
-                self.sleep(self.REFRESH_TIME)
-                continue
-
-            additional_systems = systems_res.text.splitlines()
-            PluginContext.logger.debug("Got the updated BGS data, passing it to the filter.")
-            self._filter._on_data_update(influence_data, additional_systems)
+            self.fetch()
             self.sleep(self.REFRESH_TIME)
 
-    def parse_inf_data(self, response: requests.Response) -> dict[str, dict[str, float]] | None:
+    def fetch(self):
+        PluginContext.logger.debug("Starting BGS systems data updating process...")
         try:
-            reader = csv.reader(response.text.splitlines())
-            next(reader)        # пропускаем заголовки
-        except StopIteration:
-            PluginContext.logger.error(f"The received BGS data is empty. Retrying in {self.REFRESH_TIME} seconds.")
-            return None
-        except Exception as e:
-            PluginContext.logger.error(
-                "An unexpected error occurred while processing received BGS data. "
-                f"Retrying in {self.REFRESH_TIME} seconds. Exception info:", exc_info=e
-            )
-            return None
-
-        influence_data = dict()
-        for row in reader:
-            faction, system, state, influence = tuple(row)
-            if faction not in influence_data:
-                influence_data[faction] = {}
-            influence_data[faction][system] = float(influence.replace(',', '.'))
-
-        return influence_data
+            resp = requests.get(self.FETCH_URL)
+            resp.raise_for_status()
+        except requests.RequestException as e:
+            PluginContext.logger.error("Couldn't fetch the systems list. Exception info:", exc_info=e)
+            return
+        systems = resp.text.splitlines()
+        if len(systems) == 0:
+            PluginContext.logger.error("The systems list was empty, skipping the update.")
+            return
+        self._callback(systems)
 
 
 class Filter:
-    _data: dict[str, dict[str, float]] = None       # {faction: {system: influence}}
-    _additional_systems: list[str] = None
-
     def __init__(self):
         self.__threadlock = Lock()
-        self.updater = FilterUpdater(self)
-        self.delayed_checks = Queue()
-        self.delayed_bgs = Queue()
+        self.updater = FilterUpdater(self._on_data_update)
         self.updater.start()
-
-    @property
-    def tracked_factions(self) -> list[str]:
-        return list(self._data.keys())
-
-    @property
-    def tracked_systems(self) -> list[str]:
-        from_factions = [system for faction_data in self._data.values() for system in faction_data.keys()]
-        return list(set(from_factions + self._additional_systems))
-
-    def get_faction_systems(self, faction: str) -> list[str] | None:
-        if faction not in self.tracked_factions:
-            return None
-        return self._data[faction].keys()
-
-    def remove_faction_system(self, faction: str, system: str):
-        if faction not in self.tracked_factions:
-            return
-        del self._data[faction][system]
-        PluginContext.logger.debug(f"Faction {faction}: system {system} removed from the list.")
-
-    def get_saved_influence(self, faction: str, system: str) -> float | None:
-        return self._data.get(faction, {}).get(system)
-
-    def set_saved_influence(self, faction: str, system: str, inf: float):
-        if faction not in self.tracked_factions:
-            return
-        self._data[faction][system] = inf
-        PluginContext.logger.debug(f"Faction {faction}, system {system}: saved influence set to {inf}.")
-
-    def check_local_factions(self, entry: dict):
-        if not self._data:
-            PluginContext.logger.debug(
-                f"Local factions check in {entry['StarSystem']} cannot be done: no influence data. "
-                "Saving the entry for a delayed check."
-            )
-            self.delayed_checks.put(entry)
-            return
-        with self.__threadlock:
-            system: str = entry["StarSystem"]
-            local_factions: dict[str, dict] = {f.get("Name"): f for f in entry.get("Factions", [])}
-            if None in list(local_factions.keys()):
-                PluginContext.logger.error(f"Empty faction name. Raw entry: {entry}")
-                local_factions = {k: v for k, v in local_factions.items() if k is not None}
-
-            local_tracked_factions = [f for f in local_factions if f in self.tracked_factions]
-            if local_tracked_factions:
-                PluginContext.logger.debug("Tracked factions in the system: " + ', '.join(local_tracked_factions))
-            for faction in local_tracked_factions:
-                data = local_factions[faction]
-                state = "Present"
-                for item in data.get("ActiveStates", []):
-                    if (f_state := item.get("State")) in ("Retreat", "Expansion"):
-                        state = f_state
-                saved_inf = self.get_saved_influence(faction, system)
-                current_inf = data.get("Influence", 0.0)
-                if saved_inf is None:
-                    PluginContext.logger.debug(f"Detected expansion of a tracked faction {faction} into this system.")
-                    self.report_expansion(faction, system, current_inf, state)
-                elif saved_inf != current_inf:
-                    PluginContext.logger.debug(
-                        f"Detected influence change of a tracked faction {faction}: "
-                        f"{saved_inf} -> {current_inf}."
-                    )
-                    self.report_inf_change(faction, system, current_inf, state)
-                self.set_saved_influence(faction, system, current_inf)
-
-            for tracked_f in self.tracked_factions:
-                if system in self.get_faction_systems(tracked_f) and tracked_f not in local_factions:
-                    PluginContext.logger.debug(f"Detected retreat of a tracked faction {faction} from the current system.")
-                    self.report_retreat(faction, system)
-                    self.remove_faction_system(faction, system)
+        self.bgs_reports_queue = Queue()
+        self._tracked_systems: set[str] = set()
 
     def dispatch_bgs_report(
         self,
-        affected_systems: list[str] | None,
-        affected_factions: list[str] | None,
-        url: str, params: dict
+        affected_systems: list[str],
+        url: str,
+        params: dict
     ):
-        if not affected_systems and not affected_factions:
-            raise ValueError("At least one of 'affected_factions' or 'affected_systems' must be provided.")
         if not self._data:
             PluginContext.logger.debug(
-                "BGS report check cannot be done: no tracked factions/systems data. "
-                "Saving the report for a delayed check."
+                "BGS report check cannot be done: no tracked systems data yet. Saving the report for a delayed check."
             )
-            self.delayed_bgs.put((affected_systems, affected_factions, url, params))
+            self.bgs_reports_queue.put((affected_systems, url, params))
             return
         with self.__threadlock:
-            if affected_systems and not any(s in self.tracked_systems for s in affected_systems):
+            if not any(s in self._tracked_systems for s in affected_systems):
                 PluginContext.logger.debug("None of the provided affected systems are tracked, ignoring this BGS report.")
-                return
-            if affected_factions and not any(f in self.tracked_factions for f in affected_factions):
-                PluginContext.logger.debug("None of the provided affected factions are tracked, ignoring this BGS report.")
                 return
             PluginContext.logger.debug("Sending a BGS report.")
             GoogleReporter(url, params).start()
 
-    def report_expansion(self, faction: str, system: str, inf: float, state: str):
-        url = "https://docs.google.com/forms/d/e/1FAIpQLSckFHxXulCEnxJKNS7XmY4TKrzM2eE1akxfv5XxvWdwijOzJw/formResponse?usp=pp_url"
-        params = {
-            "entry.1327463036": GameState.cmdr,
-            "entry.481092930": faction,
-            "entry.1293773018": system,
-            "entry.932945455": inf * 100,
-            "entry.319473982": state
-        }
-        GoogleReporter(url, params).start()
-
-    def report_retreat(self, faction: str, system: str):
-        url = "https://docs.google.com/forms/d/e/1FAIpQLSckFHxXulCEnxJKNS7XmY4TKrzM2eE1akxfv5XxvWdwijOzJw/formResponse?usp=pp_url"
-        params = {
-            "entry.1327463036": GameState.cmdr,
-            "entry.481092930": faction,
-            "entry.1293773018": system,
-            "entry.932945455": 0,
-            "entry.319473982": "Retreated"
-        }
-        GoogleReporter(url, params).start()
-
-    def report_inf_change(self, faction: str, system: str, inf: float, state: str):
-        url = "https://docs.google.com/forms/d/e/1FAIpQLSckFHxXulCEnxJKNS7XmY4TKrzM2eE1akxfv5XxvWdwijOzJw/formResponse?usp=pp_url"
-        params = {
-            "entry.1327463036": GameState.cmdr,
-            "entry.481092930": faction,
-            "entry.1293773018": system,
-            "entry.932945455": inf,
-            "entry.319473982": state
-        }
-        GoogleReporter(url, params).start()
-
-    def _on_data_update(self, data: dict[str, dict[str, float]], additional_systems: list[str]):
+    def _on_data_update(self, systems: list[str]):
         with self.__threadlock:
-            self._data = data
-            self._additional_systems = additional_systems
-        while not self.delayed_checks.empty():
-            PluginContext.logger.debug(f"Processing a delayed local factions check ({self.delayed_checks.qsize()} left):")
-            self.check_local_factions(self.delayed_checks.get())
-        while not self.delayed_bgs.empty():
-            PluginContext.logger.debug(f"Processing a delayed BGS report check ({self.delayed_bgs.qsize()} left):")
-            self.dispatch_bgs_report(*self.delayed_bgs.get())
+            self._tracked_systems = set(systems)
+        if not self.bgs_reports_queue.empty():
+            PluginContext.logger.debug("Processing delayed BGS report checks:")
+            while not self.bgs_reports_queue.empty():
+                self.dispatch_bgs_report(*self.bgs_reports_queue.get())
 
 
 class BGSCore(Module):
@@ -243,8 +98,6 @@ class BGSCore(Module):
             PluginContext.notifier.send(_translate("BGS module encountered an error during initialization and was disabled."), 0)
 
     def on_journal_entry(self, entry: JournalEntry):
-        if entry.data.get("event") in ("FSDJump", "Location"):
-            self.filter.check_local_factions(entry.data)
         for subm in self.submodules:
             try:
                 subm.on_journal_entry(entry.data)
---
+++ modules/bgs/submodules/faction_tracker.py
@@ -0,0 +1,206 @@
+import csv
+import requests
+from dataclasses import dataclass, fields
+from queue import Queue
+from threading import Lock
+from typing import Any, Callable, Literal
+
+from context import GameState, PluginContext
+from modules.bgs.submodule_base import Submodule
+from modules.legacy import GoogleReporter
+from modules.lib.thread import Thread
+
+FactionState = Literal['Present', 'Pending retreat', 'Retreated', 'Conflict']
+ConflictType = Literal['War', 'Election']
+ConflictStatus = Literal['active', 'pending', 'finished']
+
+
+@dataclass
+class FactionSystemData:
+    faction: str
+    system: str
+    influence: float = 0.0
+    state: FactionState = "Present"
+    conflict_type: ConflictType | None = None
+    conflict_status: ConflictStatus | None = None
+    conflict_enemy: str | None = None
+    conflict_score: int | None = None
+    conflict_score_enemy: int | None = None
+    conflict_stake: str | None = None
+    conflict_stake_enemy: str | None = None
+
+
+class FactionDataFetcher(Thread):
+    REFRESH_TIME = 30 * 60  # s
+    REMOTE_DATA_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTDY_aCGkppsZVZI_XhjBo_E3dxvGWilsOjpti9bPpqFLHM7Ar47pHfTeeSUfjLW3lI3hzsfy0YVCl7/pub?gid=1715591931&single=true&output=csv"  # noqa: E501
+
+    def __init__(self, callback: Callable[[list[list[str]]], Any]):
+        super().__init__()
+        self._callback = callback
+
+    def do_run(self):
+        while True:
+            self.fetch_data()
+            self.sleep(self.REFRESH_TIME)
+
+    def fetch_data(self):
+        try:
+            resp = requests.get(self.REMOTE_DATA_URL)
+            resp.raise_for_status()
+        except requests.RequestException as e:
+            PluginContext.logger.error("Couldn't fetch factions data. Exception info:", exc_info=e)
+            return
+        reader = csv.reader(resp.text.splitlines())
+        result = []
+        for row in reader:
+            # без активных конфликтов гугл порежет количество столбцов в csv
+            if len(row) < len(fields(FactionSystemData)):
+                row += [""] * (len(fields(FactionSystemData)) - len(row))
+            result.append(row)
+        if not result:
+            return
+        self._callback(result)
+
+
+class FactionTracker(Submodule):
+    """
+    Следит за состоянием отслеживаемых фракций в их системах,
+    сообщает при обнаружении изменений.
+    """
+
+    def __init__(self):
+        self.data: dict[str, dict[str, FactionSystemData]] = dict()      # {faction: {system: FSD}}
+        self.system_check_queue = Queue()
+        self._threadlock = Lock()
+        self._updater = FactionDataFetcher(self.on_data_update)
+        self._updater.start()
+
+    def on_journal_entry(self, entry: dict):
+        if entry["event"] not in ("Location", "FSDJump"):
+            return
+        if not self.data:
+            PluginContext.logger.debug(
+                "Local factions checks cannot be done: no factions data yet. Saving the report for a delayed check."
+            )
+            self.system_check_queue.put(entry)
+            return
+        self.check_system_factions(entry)
+
+    def check_system_factions(self, entry: dict):
+        with self._threadlock:
+            system: str = entry["StarSystem"]
+            system_factions: dict[str, dict] = {f.get("Name"): f for f in entry.get("Factions", [])}
+            if None in list(system_factions.keys()):
+                PluginContext.logger.error(f"Empty faction name. Raw entry: {entry}")
+                system_factions = {k: v for k, v in system_factions.items() if k is not None}
+
+            tracked_system_factions = [f for f in system_factions if f in self.data]
+            if tracked_system_factions:
+                PluginContext.logger.debug("Tracked factions in the system: " + ', '.join(tracked_system_factions))
+            tracked_factions_objects: list[FactionSystemData] = []
+
+            for faction in tracked_system_factions:
+                data = system_factions[faction]
+                influence = data.get("Influence")
+                if influence is None:
+                    PluginContext.logger.error(f"Empty faction influence, skipping. Raw entry: {entry}.")
+                    continue
+                state = "Present"
+                for active_state in (s.get("State") for s in data.get("ActiveStates", [])):
+                    if active_state == "Retreat":
+                        state = "Pending retreat"
+                    elif active_state in ("War", "CivilWar", "Election"):
+                        state = "Conflict"
+                tracked_factions_objects.append(FactionSystemData(faction, system, influence, state))
+
+            for faction in [name for name, systems in self.data.items() if system in systems]:
+                if faction not in [f.faction for f in tracked_factions_objects]:
+                    PluginContext.logger.debug(f"Detected retread of tracked faction '{faction}'. Reporting.")
+                    self.report_faction_data_change(FactionSystemData(faction, system, 0, "Retreated"))
+                    del self.data[faction][system]
+
+            for system_conflict in entry.get("Conflicts", []):
+                if (status := system_conflict["Status"]) == "":
+                    status = "finished"
+                match conflict_type := system_conflict["WarType"]:
+                    case "war", "civilwar": conflict_type = "War"
+                    case "election": conflict_type = "Election"
+                    case _:
+                        PluginContext.logger.warning(f"Unknown conflict type: {conflict_type}. Raw entry: {entry}")
+                        continue
+                participants = [system_conflict["Faction1"], system_conflict["Faction2"]]
+                for index, participant in enumerate(participants):
+                    faction = participant
+                    enemy = participants[index - 1]
+                    for faction_obj in tracked_factions_objects:
+                        if faction_obj.faction == faction["Name"]:
+                            faction_obj.conflict_type = conflict_type
+                            faction_obj.conflict_status = status
+                            faction_obj.conflict_enemy = enemy["Name"]
+                            faction_obj.conflict_score = faction["WonDays"]
+                            faction_obj.conflict_score_enemy = enemy["WonDays"]
+                            faction_obj.conflict_stake = faction["Stake"]
+                            faction_obj.conflict_stake_enemy = enemy["Stake"]
+                            break
+
+            for new_data in tracked_factions_objects:
+                faction = new_data.faction
+                old_data = self.data[faction].get(system)
+                if old_data == new_data:
+                    PluginContext.logger.debug(f"No data updates for tracked faction '{faction}'.")
+                    continue
+                if old_data is None:
+                    PluginContext.logger.debug(f"Detected expansion of tracked faction '{faction}'.")
+                    # так делать низзя, но этот субмодуль будет исключением
+                    self.core.filter._tracked_systems.add(system)
+                    PluginContext.logger.debug(f"System '{system}' was added to the BGS Filter list of tracked systems.")
+                elif new_data != self.data[faction][system]:
+                    PluginContext.logger.debug(f"Detected a data update for tracked faction '{faction}':")
+                    for field in fields(FactionSystemData):
+                        if (old_value := getattr(old_data, field.name)) != (new_value := getattr(new_data, field.name)):
+                            PluginContext.logger.debug(f"{field.name}: {old_value} -> {new_value}")
+                self.data[faction][system] = new_data
+                self.report_faction_data_change(new_data)
+
+    def report_faction_data_change(self, new_data: FactionSystemData):
+        url = "https://docs.google.com/forms/d/e/1FAIpQLSe04qEfF-Pj8bOcsYktryMQNaoO9ft0orOhSb3E6M_Jw2R_qQ/formResponse?usp=pp_url"
+        params = {
+            "entry.1705592545": GameState.cmdr,
+            "entry.132634971": new_data.faction,
+            "entry.719121673": new_data.system,
+            "entry.880286539": new_data.influence,
+            "entry.1723245318": new_data.state,
+            "entry.1373861537": new_data.conflict_type,
+            "entry.597895648": new_data.conflict_enemy,
+            "entry.1216403281": new_data.conflict_status,
+            "entry.807900121": new_data.conflict_score,
+            "entry.2098736877": new_data.conflict_score_enemy,
+            "entry.2070019885": new_data.conflict_stake,
+            "entry.1471364504": new_data.conflict_stake_enemy
+        }
+        GoogleReporter(url, params).start()
+
+    def on_data_update(self, new_data: list[list[str]]):
+        def clean_row(row: list[str]):
+            row = [None if item == "" else item for item in row]
+            row[2] = float(row[2])                      # influence
+            row[4] = row[4] or None                     # conflict_type
+            row[5] = row[5] or None                     # conflict_status
+            row[6] = row[6] or None                     # conflict_enemy
+            row[7] = int(row[7]) if row[7] else None    # conflict_score
+            row[8] = int(row[8]) if row[8] else None    # conflict_score_enemy
+            row[9] = row[9] or None                     # conflict_stake
+            row[10] = row[10] or None                     # conflict_stake_enemy
+            return row
+
+        with self._threadlock:
+            PluginContext.logger.debug("Received updated factions data.")
+            self.data.clear()
+            for row in new_data:
+                cleaned_row = clean_row(row)
+                fsd = FactionSystemData(*cleaned_row)
+                self.data.setdefault(fsd.faction, {})[fsd.system] = fsd
+        if not self.system_check_queue.empty():
+            PluginContext.logger.debug("Processing delayed system checks:")
+            while not self.system_check_queue.empty():
+                self.check_system_factions(self.system_check_queue.get())
