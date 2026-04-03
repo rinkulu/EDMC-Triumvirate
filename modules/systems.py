558cd4534eb85708e907de513d910add2a6b0915
rinkulu <rinkulukat@gmail.com>
2026-04-03 19:57:53 +0300

    temporary fix for galaxy-api unavailability

--- modules/systems.py
+++ modules/systems.py
@@ -1,12 +1,12 @@
+import requests
+
 import settings
-from .lib.cache import Cache
-from .lib.http import WebClient
-from .debug import debug
+from modules.debug import debug
+from modules.lib.cache import Cache
 
 
-class SystemsModule(WebClient):
+class SystemsModule:
     def __init__(self):
-        super().__init__(settings.galaxy_url)
         self.cache = Cache(max_size=1024, name="SYSTEMS_CACHE")
         self.cache.start()
 
@@ -20,19 +20,50 @@ class SystemsModule(WebClient):
             return self.cache[system]
         except KeyError:
             coords = self.fetch_system(system)
-            if coords:
+            if coords is not None:
                 self.cache[system] = coords
-                return self.cache[system]
+            return coords
 
-    def fetch_system(self, system):
+    def fetch_system(self, system: str) -> tuple[float, float, float] | None:
         try:
-            resp = self.request("GET", "/api/v1/lookup", {"name": system})
-        except Exception as e:
-            debug(f"fetch_systems failed: {e}")
-        else:
-            if resp.status_code == 200:
-                coords = resp.json()
-                return coords["x"], coords["y"], coords["z"]
+            resp = requests.get(
+                url=f"{settings.galaxy_url}/api/v1/lookup",
+                params={"name": system},
+                timeout=5,
+            )
+            resp.raise_for_status()
+            json = resp.json() or dict()
+            x, y, z = json.get("x"), json.get("y"), json.get("z")
+            if None not in (x, y, z):
+                debug(f"Got coordinates for {system} from CEC API: {x}, {y}, {z}")
+                return x, y, z  # type: ignore
             else:
-                debug(f"fetch_system failed with code {resp.status_code}")
+                debug(f"CEC API doesn't contain valid coordinates for {system}, attempting Spansh")
+        except Exception as e:
+            debug(f"Failed to retrieve coordinates from CEC API, attempting Spansh: {e}.")
+
+        try:
+            resp = requests.get(
+                url="https://www.spansh.co.uk/api/search",
+                params={"q": system},
+                timeout=5,
+            )
+            resp.raise_for_status()
+            record = next(
+                (
+                    s["record"] for s in resp.json()["results"]
+                    if s["type"] == "system"
+                    and s["record"]["name"] == system
+                ),
+                None
+            )
+            if record is None:
+                debug(f"Spansh doesn't contain a record for {system}")
                 return None
+            else:
+                x, y, z = record["x"], record["y"], record["z"]
+                debug(f"Got coordinates for {system} from Spansh: {x}, {y}, {z}")
+                return x, y, z
+        except Exception as e:
+            debug(f"Failed to retrieve coordinates from Spansh: {e}")
+            return None
